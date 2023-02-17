443131e972794847057bbedbdd708d4435994668
Tigran <abvgdesh3@gmail.com>
2023-02-17 17:53:21 +0300

    Init project.

---
+++ src/test/java/ru/tigran/ptmk_test/PtmkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tigran.ptmk_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtmkTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
